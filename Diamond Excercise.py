6606593f3686fd0b6e908a951818f7641ae23029
winter054 <winkung054@gmail.com>
2023-12-08 18:03:41 +0700

    Complete diamond exercise

--- Diamond Excercise.py
+++ Diamond Excercise.py
@@ -1,2 +1,9 @@
 width = int(input("Give Dimension of your Diamond: "))
 
+# Upper half
+for i in range (width):
+    print(" " * (width-i) + " *" * (i+1))
+ 
+# Lower Half
+for j in range (width-1):
+    print(" " * (j+2) + " *" * (width-1-j))
